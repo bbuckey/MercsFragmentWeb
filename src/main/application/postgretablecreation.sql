154d08040962966c9546736d0d23c4e75001c173
Ben Buckey <ben.buckey@gmail.com>
2013-11-07 11:28:47 -0500

    adding sql scripts

---
+++ src/main/application/postgretablecreation.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE IF NOT EXISTS base.EMPLOYEE 
+(
+    ID          integer PRIMARY KEY DEFAULT nextval('base.serial'),
+    FIRSTNAME   VARCHAR(30),
+    LASTNAME    VARCHAR(30),
+    TELEPHONE   VARCHAR(15),
+    EMAIL       VARCHAR(30),
+    datecreated date,
+    CREATED     timestamp DEFAULT current_timestamp
+);
