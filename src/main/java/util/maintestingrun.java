891bea78a87dbc917c105a73dbf1e873defba1dc
Ben Buckey <ben.buckey@gmail.com>
2013-11-08 13:44:14 -0500

    main

--- src/main/java/util/maintestingrun.java
+++ src/main/java/util/maintestingrun.java
@@ -15,7 +15,6 @@ import util.SpringBeanContextFactory;
 public class maintestingrun {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		MercManager mm = (MercManager) SpringBeanContextFactory
 				.loadXmlBeanResources().getBean("mercManager");
@@ -26,6 +25,15 @@ public class maintestingrun {
 		BossMercManager bmm = (BossMercManager) SpringBeanContextFactory
 				.loadXmlBeanResources().getBean("bossMercManager");
 
+		BossMercEntity cme = new BossMercEntity();
+		cme.setBattleFieldId(1);
+		cme.setAtk((((int)(Math.random()*100000) % 100)+ 25));
+		cme.setDef((((int)(Math.random()*100000) % 20)+10));
+		cme.setExp((((int)(Math.random()*100000) % 300) + 100));
+		cme.setGold((((int)(Math.random()*100000) % 250)+ 200));
+		cme.setHoldAtk((((int)(Math.random()*100000) % 4)+ 3));
+		cme.setHp((((int)(Math.random()*100000) % 300) + 50));
+		bmm.addBossMerc(cme);
 	/*	for (int i = 0; i <= 10; i++) {
 			ComputerMercsEntity cme = new ComputerMercsEntity();
 			cme.setBattleFieldId(1);
